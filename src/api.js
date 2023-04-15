3572e0cc996c3b4fc05e471869a2cf5d92826041
Akash <charkhaniakash@gmail.com>
2023-04-16 01:09:31 +0530

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,15 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4842eca480msh29c5748d4d9694bp18b2e7jsnaa0a3023c2b0',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "d4a2084ff378b4de559cd971504993c9"
